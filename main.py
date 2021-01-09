0c6a0b9b1f76996cbe3123d60454d98f91472868
Rahul <64806401+fangyman@users.noreply.github.com>
2021-01-09 17:22:41 -0500

    Added an image to the application.

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from tkinter import *
+from PIL import ImageTk, Image
 
 root = Tk()
 root.state('zoomed')
 root.geometry('800x600')
 
+img =  ImageTk.PhotoImage(file="windows.webp")  
 c = Canvas(root, bg='black')
 c.pack(fill="both", expand=True)
+c.create_image(60, 50, anchor=NW, image=img)
 
 root.mainloop()
