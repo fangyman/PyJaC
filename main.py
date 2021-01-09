dd03cd0bfa6f26c83d06da1a87ad410bef057bb8
David <44748556+Quietfu1@users.noreply.github.com>
2021-01-09 16:20:45 -0500

    Created the background for the application.

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("hello world")
+from tkinter import *
+
+root = Tk()
+root.geometry('800x600')
+
+c = Canvas(root, height=600, width=800, bg='black')
+c.pack(fill="both", expand=True)
+
+root.mainloop()
