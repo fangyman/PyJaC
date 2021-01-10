32c961a27a55eb4fbe6eedc150d090238dbddbd2
David <44748556+Quietfu1@users.noreply.github.com>
2021-01-10 17:08:02 -0500

    Created the function moving to simulate the movement of the image across the screen. When the image hits a wall of the screen, the image bounces accordingly.

--- main.py
+++ main.py
@@ -1,13 +1,40 @@
-from tkinter import *
+from tkinter import Tk, Canvas, NW
 from PIL import ImageTk, Image
 
 root = Tk()
+root.update()
+root.title('Bouncing DVD')
 root.state('zoomed')
 root.geometry('800x600')
 
-img =  ImageTk.PhotoImage(file="windows.webp")  
+width = root.winfo_width()
+height = root.winfo_height()
+
+img = ImageTk.PhotoImage(file="bouncing_dvd_logo.jpg")
+
 c = Canvas(root, bg='black')
 c.pack(fill="both", expand=True)
-c.create_image(60, 50, anchor=NW, image=img)
+
+image = c.create_image(60, 50, anchor=NW, image=img)  # Main image
+
+x_move = 3
+y_move = 3
+
+
+def moving():
+    '''
+    Moves the image by changing its coordinates at incremental values. When the image hits one of the sides of the screen
+    the value becomes negative.
+    '''
+    global x_move, y_move
+    c.move(image, x_move, y_move)
+    if c.coords(image)[0] + img.width() >= width or c.coords(image)[0] <= 0:
+        x_move = -x_move
+    if c.coords(image)[1] + img.height() >= height or c.coords(image)[1] <= 0:
+        y_move = -y_move
+    root.after(8, moving)
+
+
+moving()
 
 root.mainloop()
