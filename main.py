59ccf6379b92bfffb792d1d31e87799ad78bab13
David <44748556+Quietfu1@users.noreply.github.com>
2021-01-13 20:03:20 -0500

    Moved sliders and buttons to a menu area. Fixed an issue where changing the x or y move speed to 0 would make the new speed always positive (instead of remembering the direction it was previously moving in).

--- main.py
+++ main.py
@@ -14,23 +14,23 @@ image = pygame.image.load('dvd.jpg')  # loads up the image
 
 CONSTANTS = {'x_move': 3,
              'y_move': 3,
-             'x': randint(0, screen_size.get_width() - image.get_width()),
+             'x': randint(0, screen_size.get_width() - image.get_width() - 100),
              'y': randint(0, screen_size.get_height() - image.get_height())}  # constants
 
 black = (0, 0, 0)  # background color to fill in rgb values
 timer = pygame.time.Clock()  # clock to update the frames
-xslider = Slider(root, 20, 0, 60, 40, min=0,
-                 max=50, step=1, colour=(255, 0, 0), handleColour=(255, 255, 255), handleRadius=15, text="x",
-                 textColor=(255, 255, 255))
+xslider = Slider(root, screen_size.get_width() - 80, 50, 60, 40, min=0,
+                 max=50, step=1, colour=(255, 0, 0), handleColour=(255, 255, 255), handleRadius=15)
 yslider = Slider(root, screen_size.get_width() - 80, 0, 60, 40, min=0,
-                 max=50, step=1, colour=(255, 0, 0), handleColour=(255, 255, 255), handleRadius=15, text="y speed",
-                 textColor=(255, 255, 255))
-quitButton = Button(root, screen_size.get_width() - 80, screen_size.get_height() - 80, 50, 50,  text='Quit',
+                 max=50, step=1, colour=(255, 0, 0), handleColour=(255, 255, 255), handleRadius=15)
+quitButton = Button(root, screen_size.get_width() - 100, screen_size.get_height() - 80, 100, 50,  text='Quit',
                      fontSize=20, margin=20, inactiveColour=(250, 0, 0), radius=20, onClick=lambda: pygame.quit())
 
 
-fileButton = Button(root, 20, screen_size.get_height() - 80, 100, 60,  text='File Select',
+fileButton = Button(root, screen_size.get_width() - 100, screen_size.get_height() - 150, 100, 60,  text='File Select',
                      fontSize=20, margin=10, inactiveColour=(250, 0, 0), radius=20)
+
+
 
 def moving():
     '''
@@ -38,7 +38,7 @@ def moving():
     the value becomes negative.
     '''
     global CONSTANTS
-    if CONSTANTS['x'] + image.get_width() >= screen_size.get_width() or CONSTANTS['x'] <= 0:
+    if CONSTANTS['x'] + image.get_width() + 100 >= screen_size.get_width() or CONSTANTS['x'] <= 0:
         CONSTANTS['x_move'] = -CONSTANTS['x_move']
     if CONSTANTS['y'] + image.get_height() >= screen_size.get_height() or CONSTANTS['y'] <= 0:
         CONSTANTS['y_move'] = -CONSTANTS['y_move']
@@ -49,29 +49,45 @@ def moving():
 
 while True:
     root.fill(black)
+    pygame.draw.rect(root, (255,255,255), pygame.Rect(screen_size.get_width() - 100, 0, 100, screen_size.get_height())) 
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             quit()
-        # or MOUSEBUTTONDOWN depending on what you want.
-        if event.type == pygame.MOUSEBUTTONUP:
-            print(event.pos)
 
     events = pygame.event.get()
     xslider.listen(events)
     xslider.draw()
     if CONSTANTS['x_move'] > 0:
         CONSTANTS['x_move'] = xslider.getValue()
+        if xslider.getValue() != 0:
+            last_x = CONSTANTS['x_move']
+    elif CONSTANTS['x_move'] == 0 and xslider.getValue() > 0:
+        if last_x > 0:
+            CONSTANTS['x_move'] = xslider.getValue()
+        else:
+            CONSTANTS['x_move'] = -xslider.getValue()
     else:
         CONSTANTS['x_move'] = -xslider.getValue()
+        if xslider.getValue() != 0:
+            last_x = CONSTANTS['x_move']
 
     yslider.listen(events)
     yslider.draw()
     if CONSTANTS['y_move'] > 0:
         CONSTANTS['y_move'] = yslider.getValue()
+        if yslider.getValue() != 0:
+            last_y = CONSTANTS['y_move']
+    elif CONSTANTS['y_move'] == 0 and yslider.getValue() > 0:
+        if last_y > 0:
+            CONSTANTS['y_move'] = yslider.getValue()
+        else:
+            CONSTANTS['y_move'] = -yslider.getValue()
     else:
         CONSTANTS['y_move'] = -yslider.getValue()
+        if yslider.getValue() != 0:
+            last_y = CONSTANTS['y_move']
     quitButton.listen(events)
     quitButton.draw()
     fileButton.listen(events)
